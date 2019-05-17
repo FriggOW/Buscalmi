0a4e496bc76f9c53fbd6be1348942a406069e7f2
FriggOW <jonandergutierrez@gmail.com>
2019-05-17 22:06:59 +0200

    BBDD Connection

--- app/src/main/java/com/browser/buscalmi/Fragmentos/FragInicio.java
+++ app/src/main/java/com/browser/buscalmi/Fragmentos/FragInicio.java
@@ -2,8 +2,8 @@ package com.browser.buscalmi.Fragmentos;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,16 +12,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
 import com.browser.buscalmi.Adaptadores.RecyclerAdapter;
+import com.browser.buscalmi.AllProductos;
+import com.browser.buscalmi.Apollo.MiApolloClient;
 import com.browser.buscalmi.Producto;
 import com.browser.buscalmi.R;
 
 import java.util.ArrayList;
 
+import javax.annotation.Nonnull;
 
-public class FragInicio extends Fragment{
 
-    private  RecyclerView listTopVentas;
+public class FragInicio extends Fragment {
+
+    private RecyclerView listTopVentas;
     private RecyclerView listTopPrecios;
 
     private static ArrayList<Producto> productos = new ArrayList<Producto>();
@@ -42,29 +49,61 @@ public class FragInicio extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag_inicio, container, false);
-
+        /* TEST PRODUCTS
         productos.add(new Producto("Nintendo Switch", 300));
         productos.add(new Producto("Pokemon Sword", 60));
         productos.add(new Producto("Pokemon Shield", 60));
         productos.add(new Producto("Tarjeta de Google", 5));
         productos.add(new Producto("Overwatch", 20));
         productos.add(new Producto("The Witcher", 30));
+        */
+
+        initializeProducts();
+
+        //Esperando ala respuesta de la conexion de la BBDD
+        SystemClock.sleep(1000);
 
         listTopVentas = view.findViewById(R.id.TopVentas);
         listTopVentas.setLayoutManager(new GridLayoutManager(getContext(), 3));
         productosTopVentas.addAll(productos);
-        RecyclerAdapter b=new RecyclerAdapter(productosTopVentas, getContext());
+        RecyclerAdapter b = new RecyclerAdapter(productosTopVentas, getContext());
         listTopVentas.setAdapter(b);
 
         listTopPrecios = view.findViewById(R.id.TopPrecios);
         listTopPrecios.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
         productosTopPrecio.addAll(productos);
-        RecyclerAdapter c=new RecyclerAdapter(productosTopPrecio, getContext());
+        RecyclerAdapter c = new RecyclerAdapter(productosTopPrecio, getContext());
         listTopPrecios.setAdapter(c);
 
         return view;
     }
 
+    private void initializeProducts() {
+
+        MiApolloClient.getApolloClient().query(AllProductos.builder()
+                .build())
+                .enqueue(new ApolloCall.Callback<AllProductos.Data>() {
+
+                    @Override
+                    public void onResponse(@Nonnull Response<AllProductos.Data> response) {
+
+                        for (int i = 0; i < response.data().productos().size(); i++){
+
+                            productos.add(new Producto(
+                                    response.data().productos().get(i).nombre(),
+                                    Integer.valueOf(""+response.data().productos().get(i).precio()))
+                            );
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(@Nonnull ApolloException e) {
+
+                    }
+                });
+
+
+    }
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
